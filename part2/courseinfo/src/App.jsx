900311a4eb507715ed602b3ba86dde36f575ed7a
ulasaydin <ulassedataydin@gmail.com>
2024-03-21 14:33:39 +0200

    Implement 2.1

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,63 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (  
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+}
+ 
+const Content = (props) => {
+  return (
+    <div>
+      {props.parts.map(part => <Part key={part.id} part={part.name} exercises={part.exercises} />)}
+    </div>
+  );
+};
+ 
+const Course = (props) => {
+  return (
+    <div>
+      <Header course={props.course.name} />
+      <Content parts={props.course.parts} />
+    </div>
+  );
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      // {
+      //   name: 'Test part',
+      //   exercises: 4,
+      //   id: 4
+      // }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
