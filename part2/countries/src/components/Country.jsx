d824e057e16a51d34d21a6a9c9dd700812e820ac
ulasaydin <ulassedataydin@gmail.com>
2024-04-16 18:25:19 +0300

    Implement 2.20

--- part2/countries/src/components/Country.jsx
+++ part2/countries/src/components/Country.jsx
@@ -1,6 +1,18 @@
+import axios from 'axios';
 import React from 'react';
+import { useState } from 'react';
 
 const Country = ({ country }) => {
+    const api_key = import.meta.env.VITE_API_KEY;
+    const [weather, setWeather] = useState(null)
+
+    useState(() => {
+      axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${country.capital[0]}&appid=${api_key}&units=metric`)
+            .then(response => {
+                setWeather(response.data);
+            })
+    } , [api_key, country.capital[0]]);
+
     return (
         <div>
             <h1>{country.name.common}</h1>
@@ -12,7 +24,16 @@ const Country = ({ country }) => {
                     <li key={language}>{language}</li>
                 ))}
             </ul>
-            <img src={country.flags.png} alt={country.name.common} width="100" />
+            <img src={country.flags.png} alt={country.name.common} width="200" />
+            {weather && (
+                <div>
+                    <h2>Weather in {country.capital[0]}</h2>
+                    <div><b>temperature:</b> {weather.main.temp} Celsius</div>
+                    <div><b>description:</b> {weather.weather[0].description}</div>
+                    <div><img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`} width = "100" alt={weather.weather[0].description} /></div>
+                    <div><b>wind:</b> {weather.wind.speed} m/s</div>
+                </div>
+            )}
         </div>
     );
 }
