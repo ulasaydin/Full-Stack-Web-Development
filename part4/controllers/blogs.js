6d0551d2274e5f1b090c9f0fc8bb2a7c497299b2
ulasaydin <ulassedataydin@gmail.com>
2024-05-15 13:08:29 +0300

    Implement 4.10

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -6,14 +6,11 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
-
-  blog
+  const savedBlog = await blog
     .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  response.status(201).json(savedBlog)
 })
 
 module.exports = blogsRouter
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -34,6 +34,26 @@ test('unique identifier property of the blog posts is named id', async () => {
   })
 })
 
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  const titles = response.body.map(r => r.title)
+  expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+  expect(titles).toContain('Canonical string reduction')
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
