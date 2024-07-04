0988d3566ece8a637e061c0f8aa3dae482732b9d
ulasaydin <ulassedataydin@gmail.com>
2024-07-04 17:32:47 +0300

    Implement 5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -26,3 +26,33 @@ test('renders content', () => {
   expect(urlElement).not.toBeInTheDocument()
   expect(likesElement).not.toBeInTheDocument()
 })
+
+test('clicking the button shows the url and likes', async () => {
+  const blog = {
+    title: 'Component testing is done with react-testing-library',
+    author: 'Arto Hellas',
+    likes: 10,
+    url: 'http://example.com',
+    user: { id: '1', name: 'Tester', username: 'tester' },
+  }
+  const user = {
+    username: 'tester',
+  }
+
+  render(<Blog user={user} blog={blog} />)
+
+  let urlElement = screen.queryByText(blog.url)
+  let likesElement = screen.queryByText(`likes ${blog.likes}`)
+  
+  expect(urlElement).not.toBeInTheDocument()
+  expect(likesElement).not.toBeInTheDocument()
+
+  const button = screen.getByText('view')
+  await button.click()
+  
+  urlElement = screen.queryByText(blog.url)
+  likesElement = screen.queryByText(`likes ${blog.likes}`)
+  
+  expect(urlElement).toBeInTheDocument()
+  expect(likesElement).toBeInTheDocument()
+})
