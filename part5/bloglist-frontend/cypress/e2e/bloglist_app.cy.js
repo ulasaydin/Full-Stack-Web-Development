2cb924c70a859fa86c21a498f5c741d9b29ff13a
ulasaydin <ulassedataydin@gmail.com>
2024-07-05 13:36:28 +0300

    Implement 5.18

--- part5/bloglist-frontend/cypress/e2e/bloglist_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/bloglist_app.cy.js
@@ -1,6 +1,12 @@
 describe('Bloglist app', function() {
   beforeEach(function() {
     cy.visit('http://localhost:5173')
+    // const user = {
+    //   name: 'Matti Luukkainen',
+    //   username: 'testuser',
+    //   password: 'testpassword'
+    // }
+    // cy.request('POST', 'http://localhost:5173/api/users/', user)
   })
 
   it('Login form is shown', function() {
@@ -8,5 +14,25 @@ describe('Bloglist app', function() {
     cy.contains('username')
     cy.contains('password')
   })
+
+  it('succeeds with correct credentials', function() {
+    cy.contains('Log in to application').click()
+    cy.get('#username').type('testuser')
+    cy.get('#password').type('testpassword')
+    cy.get('#login-button').click()
+
+    cy.contains('Matti Luukkainen logged in')
+  })
   
+  it('fails with wrong credentials', function() {
+    cy.contains('Log in to application').click()
+    cy.get('#username').type('testuser')
+    cy.get('#password').type('wrongpassword')
+    cy.get('#login-button').click()
+
+    cy.get('.error')
+      .should('contain', 'wrong username or password')
+      .and('have.css', 'color', 'rgb(255, 0, 0)')
+    cy.get('html').should('not.contain', 'Matti Luukkainen logged in')
+  })
 })
